10848e5bf3b49f30d2dc6eebd0187af44d26fae2
Gabriele Tronchin <gabriele.tronchin@reti.it>
2023-11-09 01:57:41 +0100

    Add sample controller

---
+++ src/ApiApplication/Controllers/ShowtimeController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
+
+namespace ISP.TicketConnector.API.Controllers.Buildings
+{
+    [Route("[controller]")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class ShowtimeController : Controller
+    {
+        private readonly ILogger<ShowtimeController> _logger;
+
+        public ShowtimeController(ILogger<ShowtimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("ById/{id:required}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetById([FromRoute] string id)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Post([FromBody] object payload)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                return CreatedAtAction(nameof(GetById), new { id = 1 }, payload);
+            }
+            catch (ArgumentNullException e)
+            {
+                _logger.LogError($"{nameof(Post)}", e);
+
+                return StatusCode(StatusCodes.Status400BadRequest, e);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"{nameof(Post)}", e);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        public async Task<IActionResult> Put([FromBody] object payload)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                return AcceptedAtAction(nameof(GetById), new { name =1}, payload);
+            }
+            catch (ArgumentNullException e)
+            {
+                _logger.LogError($"{nameof(Put)}", e);
+
+                return StatusCode(StatusCodes.Status400BadRequest, e);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"{nameof(Put)}", e);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
+
+        [HttpDelete("{id:required}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"{nameof(Delete)}", e);
+
+                return StatusCode(StatusCodes.Status500InternalServerError,e);
+            }
+        }
+    }
+}
