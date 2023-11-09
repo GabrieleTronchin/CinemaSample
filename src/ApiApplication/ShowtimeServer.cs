82a187664b623cf5fe561c1cbbfd47012102274b
Gabriele Tronchin <gabriele.tronchin@reti.it>
2023-11-09 15:42:38 +0100

    Implement raw GRPC server

--- src/ApiApplication/ShowtimeServer.cs
+++ src/ApiApplication/ShowtimeServer.cs
@@ -2,25 +2,62 @@
 using ApiApplication.Database.Entities;
 using ApiApplication.Database.Repositories.Abstractions;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
 namespace ShowTimeProto
 {
 
-    public class ShowtimeServer  
+    public class ShowtimeServer : ShowTimeApi.ShowTimeApiBase
     {
         private readonly IShowtimesRepository _showtimesRepository;
+        private readonly ILogger<ShowtimeServer> _logger;
 
-        public ShowtimeServer(IShowtimesRepository showtimesRepository)
+        public ShowtimeServer(ILogger<ShowtimeServer> logger,IShowtimesRepository showtimesRepository)
         {
             _showtimesRepository = showtimesRepository;
+            _logger = logger;
         }
 
-        public  async Task CreateShowtime()
+        public override async Task<responseModel> CreateShowTime(ShowtimeCreationRequest request, ServerCallContext context)
         {
-            throw new NotImplementedException();
+            try
+            {
+                //TODO refactor completly move on a single domain model and add also a API
+
+                ShowtimeEntity showtimeEntity = new()
+                {
+                    AuditoriumId = request.AuditoriumId,
+                    Movie = new MovieEntity()
+                    {
+                        ImdbId = request.Movie.ImdbId,
+                        Title = request.Movie.Title,
+                        ReleaseDate = request.Movie.ReleaseDate.ToDateTime(),
+                        Stars = request.Movie.Stars,
+                    },
+                    SessionDate = request.SessionDate.ToDateTime(),
+                };
+                var t = context.CancellationToken;
+
+                var ret = await _showtimesRepository.CreateShowtime(showtimeEntity, t);
+
+                return new responseModel() { Success = true }; //TODO Fix return
+            }
+            catch (Exception ex)
+            {
+                //TODO Fix return
+
+                _logger.LogError(ex, $"An error occurred at {nameof(ShowtimeServer)}");
+                var errorRetModel = new responseModel() { Success = false  };
+                errorRetModel.Exceptions.Add(new moviesApiException() { Message = ex.Message, StatusCode = 500 });
+
+                return errorRetModel; 
+            }
+
         }
+
+
     }
 
 }
