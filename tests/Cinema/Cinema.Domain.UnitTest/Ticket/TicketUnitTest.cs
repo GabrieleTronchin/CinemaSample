78f0e8d64e2a8f13350e16057a51a98b136b7e6a
Gabriele Tronchin <gabriele.tronchin@reti.it>
2023-11-12 16:13:27 +0100

    Add sample unit test prj

---
+++ tests/Cinema/Cinema.Domain.UnitTest/Ticket/TicketUnitTest.cs
@@ -0,0 +1,26 @@
+using Bogus;
+using Cinema.Domain.Ticket;
+
+namespace Cinema.Domain.UnitTest.Ticket
+{
+    public class TicketUnitTest
+    {
+        [Fact]
+        public void CreateTicketTest()
+        {
+
+            var ticket = TicketEntity.Create(Utility.GenerateSeats(28, 22), Guid.NewGuid(), "My Test Movie");
+
+            Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void CreateTicketTest_NotAllowed()
+        {
+
+           Assert.Throws<ArgumentException>(() => TicketEntity.Create(new List<Seat>(), Guid.NewGuid(), "My Test Movie"));
+
+        }
+
+    }
+}
---
+++ tests/Cinema/Cinema.Domain.UnitTest/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Domain.UnitTest;
+
+public class Utility
+{
+    public static List<Seat> GenerateSeats(short rows, short seatsPerRow)
+    {
+        var seats = new List<Seat>();
+        for (short r = 1; r <= rows; r++)
+            for (short s = 1; s <= seatsPerRow; s++)
+                seats.Add(new Seat(r, s));
+        return seats;
+    }
+}
